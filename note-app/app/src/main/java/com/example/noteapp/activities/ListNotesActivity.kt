a072a0226d6be1b4376b65eec09f04784f762cc8
Lucas Breur <lucasbreur@gmail.com>
2020-10-24 22:06:05 -0300

    Configured room runtime & kapt 2.2.5

--- note-app/app/src/main/java/com/example/noteapp/activities/ListNotesActivity.kt
+++ note-app/app/src/main/java/com/example/noteapp/activities/ListNotesActivity.kt
@@ -1,12 +1,16 @@
 package com.example.noteapp.activities
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.preference.PreferenceManager
 import com.example.noteapp.R
 import com.example.noteapp.models.NoteSingleton
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_list_notes.*
 import kotlinx.android.synthetic.main.note_card.view.*
 
@@ -24,12 +28,26 @@ class ListNotesActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
+        val sharedPrefs =
+                getSharedPreferences("Users", Context.MODE_PRIVATE)
+
+        val username = sharedPrefs.getString("username", "")
+
+        Toast.makeText(this, "Logged in as $username", Toast.LENGTH_LONG).show()
+
+        // 'Fab' is a placeholder button, it is being triggered by "on resume"
+        // Snackbar.make(fab, "Logged in as $username", Snackbar.LENGTH_LONG).show()
+
         constructNotes()
     }
 
     fun constructNotes() {
 
         noteContainer.removeAllViews()
+
+        // Get default preferences file for the app
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val color = prefs.getInt("noteColor", R.color.noteDefaultColor)
 
         for (note in NoteSingleton.list) {
             val card =
@@ -38,6 +56,7 @@ class ListNotesActivity : AppCompatActivity() {
             card.txtTitle.text = note.title
             card.txtDescription.text = note.description
             card.txtUser.text = note.user
+            card.setBackgroundColor(color)
 
             noteContainer.addView(card)
         }
@@ -53,6 +72,12 @@ class ListNotesActivity : AppCompatActivity() {
 
         if (item.itemId == R.id.user) {
             val intent = Intent(this, UserActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+
+        if (item.itemId == R.id.config) {
+            val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
             return true
         }
