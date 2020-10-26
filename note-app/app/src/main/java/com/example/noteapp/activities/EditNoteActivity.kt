fbb04c9277f8aa92ed85b5d023c8132deb0635e2
Lucas Breur <lucasbreur@gmail.com>
2020-10-25 23:18:46 -0300

    Added color picker to edit screen

--- note-app/app/src/main/java/com/example/noteapp/activities/EditNoteActivity.kt
+++ note-app/app/src/main/java/com/example/noteapp/activities/EditNoteActivity.kt
@@ -1,15 +1,23 @@
 package com.example.noteapp.activities
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.room.Room
 import com.example.noteapp.R
 import com.example.noteapp.db.AppDatabase
 import com.example.noteapp.models.Note
+import com.jaredrummler.android.colorpicker.ColorPickerDialog
+import com.jaredrummler.android.colorpicker.ColorPickerDialogListener
 import kotlinx.android.synthetic.main.activity_edit_note.*
+import kotlinx.android.synthetic.main.activity_edit_note.btnColorPicker
+import kotlinx.android.synthetic.main.activity_edit_note.etDescription
+import kotlinx.android.synthetic.main.activity_edit_note.etTitle
+import kotlinx.android.synthetic.main.activity_new_note.*
 
-class EditNoteActivity : AppCompatActivity() {
+class EditNoteActivity : AppCompatActivity(), ColorPickerDialogListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_note)
@@ -19,7 +27,22 @@ class EditNoteActivity : AppCompatActivity() {
         etTitle.setText(note.title)
         etDescription.setText(note.description)
 
+        btnColorPicker.setOnClickListener {
+            ColorPickerDialog.newBuilder().show(this)
+        }
+
         btnSaveEdit.setOnClickListener {
+
+            val sharedPrefs =
+                getSharedPreferences("Users", Context.MODE_PRIVATE)
+
+            val background = sharedPrefs.getInt("background", 0)
+
+            if (background != 0) {
+                note.background = background
+                resetBackground(sharedPrefs)
+            }
+
             // Assign updated field values to the note
             note.title = etTitle.text.toString()
             note.description = etDescription.text.toString()
@@ -40,4 +63,27 @@ class EditNoteActivity : AppCompatActivity() {
         }
 
     }
+
+    fun resetBackground(sharedPrefs : SharedPreferences) {
+        val editor = sharedPrefs.edit()
+
+        // Resets to 0 for the next insertion
+        editor.putInt("background", 0)
+
+        editor.commit() // Writes on and closes file (Apply keeps the file open)
+    }
+
+    override fun onColorSelected(dialogId: Int, color: Int) {
+        val sharedPrefs =
+            getSharedPreferences("Users", Context.MODE_PRIVATE)
+
+        val editor = sharedPrefs.edit()
+
+        // Temporarily saves the picked color to be retrieved and saved in the database when the users concludes the card editing
+        editor.putInt("background", color)
+//        Toast.makeText(this, "Selected color: " + color.toString(), Toast.LENGTH_LONG).show()
+        editor.commit() // Writes on and closes file (Apply keeps the file open)
+    }
+
+    override fun onDialogDismissed(dialogId: Int) { }
 }
