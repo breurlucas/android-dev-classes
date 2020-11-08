b43d0b208aa70d037f7fad85e9840527b605915c
Lucas Breur <lucasbreur@gmail.com>
2020-11-08 18:42:20 -0300

    Implemented the updateScreen method

--- retrofit-ex-app/app/src/main/java/com/example/retrofit_ex_app/activities/ProductListActivity.kt
+++ retrofit-ex-app/app/src/main/java/com/example/retrofit_ex_app/activities/ProductListActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import com.example.retrofit_ex_app.R
 import com.example.retrofit_ex_app.models.Product
 import kotlinx.android.synthetic.main.activity_product_list.*
+import kotlinx.android.synthetic.main.product_card.view.*
+import java.text.NumberFormat
 
 /* Ideally we should use a recycle view instead of the dynamically loaded cards as we are
 implementing in this app; the recycle view element provides better performance.
@@ -18,10 +20,24 @@ class ProductListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_product_list)
     }
 
-    fun updateScreen(products: List<Product>) {
+    fun updateScreen(products: List<Product>?) {
 
-        for(product in products) { // For each (Java)
-            val card = layoutInflater.inflate(R.layout.product_card, container, false)
+        /* We clear and load all cards each time we update the screen. With a large number of
+        entries this can lead to poor performance of the application. This issue is solved by the
+        recycle view. */
+        container.removeAllViews()
+
+        val formatter = NumberFormat.getCurrencyInstance()
+
+        products?.let {
+            for(product in it) { // For each (Java)
+                val card = layoutInflater.inflate(R.layout.product_card, container, false)
+
+                card.txtName.text = product.nomeProduto
+                card.txtPrice.text = formatter.format(product.precProduto)
+
+                container.addView(card)
+            }
         }
     }
 }
